15d90378464bdb625dd89d958ec26eddd7ba3e8a
Anh Nguyen <anhnguyenvn@live.com>
2015-09-19 12:10:03 -0400

    [Dev] Add 2 view controller (login + set initial location), layout is still messy

---
+++ Project/EventHub/EventHub/SetLocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SetLocationViewController.swift
+//  EventHub
+//
+//  Created by Anh Nguyen on 9/19/15.
+//  Copyright (c) 2015 Eagle-team. All rights reserved.
+//
+
+import UIKit
+
+class SetLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
